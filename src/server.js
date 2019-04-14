3ed106ea5f4c2d8971bfe31c1b61aa37fbdc9e81
Will Xavier <willyelnsg.x@gmail.com>
2019-04-14 06:42:18 -0300

    Add env variables

--- src/server.js
+++ src/server.js
@@ -41,4 +41,4 @@ app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 // Adicionando as rotas
 app.use(require("./routes"));
 
-server.listen(3333);
+server.listen(process.env.PORT || 3333);
